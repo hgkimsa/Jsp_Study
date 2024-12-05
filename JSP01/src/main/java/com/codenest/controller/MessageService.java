554fc18206fba776469b0d979f6435cfe0af7c85
hgkimsa <gmlrhkd432@gmail.com>
2024-12-05 17:09:43 +0900

    커밋

--- JSP01/src/main/java/com/codenest/controller/MessageService.java
+++ JSP01/src/main/java/com/codenest/controller/MessageService.java
@@ -14,32 +14,29 @@ import com.codenest.model.todoDTO;
 
 @WebServlet("/MessageService")
 public class MessageService extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// 1. 한글 인코딩
-		request.setCharacterEncoding("UTF-8");
-		
-		
-		String google_account = request.getParameter("google_account");
-		String startdate = request.getParameter("todo_startdate");
-		String deaddate = request.getParameter("todo_deaddate");
-		String memo = request.getParameter("todo_memo");
-		String file = request.getParameter("todo_FILE_PATH");
-		String created_at = request.getParameter("created_at");
-		
-		// 3. 데이터 처리하기 -> DB 전달
-		todoDTO dto = new todoDTO();
-		dto.setGoogle_account(google_account);
-		dto.setTodo_startdate(startdate);
-		dto.setTodo_deaddate(deaddate);
-		dto.setTodo_memo(memo);
-		dto.setTodo_file_path(file);
-		dto.setCreated_at(created_at);
-		
-		// 4. 결과에 따른 화면 출력 작업
-		RequestDispatcher rd = request.getRequestDispatcher("Message.jsp");
-		rd.forward(request, response);
-	}
-
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 1. 한글 인코딩 설정
+        request.setCharacterEncoding("UTF-8");
+        
+        // 2. 요청에서 파라미터를 가져옵니다.
+        String google_account = request.getParameter("google_account");
+        String startdate = request.getParameter("todo_startdate");
+        String deaddate = request.getParameter("todo_deaddate");
+        String memo = request.getParameter("todo_memo");
+        String file = request.getParameter("todo_FILE_PATH");
+        
+        // 3. 데이터 처리하기 -> DB 전달
+        todoDTO dto = new todoDTO();
+        dto.setGoogle_account(google_account); // 구글 계정 설정
+        dto.setTodo_startdate(startdate);       // 시작일 설정
+        dto.setTodo_deaddate(deaddate);         // 마감일 설정
+        dto.setTodo_memo(memo);                 // 메모 설정
+        dto.setTodo_file_path(file);            // 파일 경로 설정
+        
+        // 4. 결과에 따른 화면 출력 작업
+        RequestDispatcher rd = request.getRequestDispatcher("Message.jsp"); // Message.jsp로 포워드
+        rd.forward(request, response); // 요청과 응답을 포워드하여 다음 페이지로 이동
+    }
 }
