22b48d66f1ff9eb27f10edc6913d9fa07caa90aa
gmlrh <gmlrh@192.168.219.102>
2024-12-18 07:18:43 +0900

    mySQL 설치완료

---
+++ WebProj/src/main/webapp/mysql/product.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS product(
+    p_id VARCHAR(10) NOT NULL,
+    p_name VARCHAR(10),
+    p_unitPrice INTEGER,
+    p_description TEXT,
+    p_category VARCHAR(20),
+    p_manufacturer VARCHAR(20),
+    p_unitsInStock LONG,
+    p_condition VARCHAR(20),
+    p_fileName VARCHAR(20),
+    PRIMARY KEY (p_id)
+) DEFAULT CHARSET=utf8;
+
+ALTER TABLE product MODIFY COLUMN p_name VARCHAR(20);
