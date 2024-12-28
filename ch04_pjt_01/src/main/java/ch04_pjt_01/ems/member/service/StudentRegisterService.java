854e6993acacba67817e86ae94bbad2d79ce6e0f
gmlrh <gmlrh@192.168.219.102>
2024-12-28 11:44:27 +0900

    패키지 이동

--- ch04_pjt_01/src/main/java/ch04_pjt_01/ems/member/service/StudentRegisterService.java
+++ ch04_pjt_01/src/main/java/ch04_pjt_01/ems/member/service/StudentRegisterService.java
@@ -1,18 +1,19 @@
-package ch04_pjt_01.ems.member.dao;
+package ch04_pjt_01.ems.member.service;
 
 import ch04_pjt_01.ems.member.Student;
+import ch04_pjt_01.ems.member.dao.StudentDao;
 
 public class StudentRegisterService {
-
+	
 	private StudentDao studentDao;
-
+	
 	public StudentRegisterService(StudentDao studentDao) {
 		this.studentDao = studentDao;
 	}
 	
 	public void register(Student student) {
-		if(verify(student.getsName())) {
-			StudentDao.insert(student);
+		if(verify(student.getsNum())) {
+			studentDao.insert(student);
 		} else {
 			System.out.println("The student has already registered.");
 		}
@@ -20,6 +21,6 @@ public class StudentRegisterService {
 	
 	public boolean verify(String sNum) {
 		Student student = studentDao.select(sNum);
-		return student == null ? true : false;
+		return student == null? true : false;
 	}
 }
