1ce68f36d92547f3aeae6aa0354a5622a7525a86
User <User@192.168.219.103>
2025-02-17 18:46:18 +0900

    commit

---
+++ JSP_Study/src/main/java/ch05/LoginServlet.java
@@ -0,0 +1,37 @@
+package ch05;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		
+		String id = request.getParameter("id");
+		String pwd = request.getParameter("pwd");
+		
+		PrintWriter out = response.getWriter();
+		out.print("<html><body>");
+		out.print("당신이 입력한 정보입니다.<br>");
+		out.print("아이디 : ");
+		out.print(id);
+		out.print("<br> 비밀번호 : ");
+		out.print(pwd);
+		
+		out.println("<br><a href='javascript:history.go(-1)'>다시</a>");
+		out.print("</body></html>");
+		out.close();
+	}
+}
